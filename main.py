a7ad5bf5ce62c58cbb47c9d69283bb198ded213f
Anni <156410258+annirollirepublic@users.noreply.github.com>
2026-05-24 10:19:46 +0200

    Added Empty Classes with docstrings

---
+++ main.py
@@ -0,0 +1,36 @@
+class Habit:
+    """
+    Object that represents the actual habit. It contains all the crucial information about the habit, that determines the subsequent business logic.
+    It is the only class object that can be instantiated by the user.
+    Args: habit_name: string, period: Period, start_date: datetime (today by default)
+    """
+
+class HabitRepository:
+    """
+    Object that contains all the management logics of the habits and provides an interface to the database where the habit data is persisted.
+    """
+
+class Task:
+    """
+    Object that represents the actual, tick-off tasks that arise from a corresponding habit and its defined period.
+    Tasks are created by the app, given the information from the habit. The user cannot create tasks independently.
+    """
+
+class TaskRepository:
+    """
+    Object that contains all the management logics of the tasks and provides an interface to the database where the task data is persisted.
+    Tasks records will be deleted, once they are checked off and the next follow-up task will be created
+    """
+
+class CompletionRecord:
+    """
+    Object that is generated, when triggered by the task completion as described above.
+    They do contain a reference to the corresponding habit, as well as the completion date and status.
+    They represent the actual historical data and cannot be changed by the user directly.
+    """
+
+class CompletionRecordRepository:
+    """
+    Object that contains all the management logics of the completion records and provides an interface to the database where the task data is persisted.
+    All further analysis can be calculated from this class.
+    """
