e66db772472878262035b363ac5aedb988563fbe
Anni <156410258+annirollirepublic@users.noreply.github.com>
2026-07-20 23:22:40 +0200

    Finalized Documentation analyzer module

--- source/app_logic/analyzer_modules.py
+++ source/app_logic/analyzer_modules.py
@@ -31,14 +31,18 @@ class RecordAnalyzer:
     """
 
     def __init__(self, record_repo: CompletionRecordRepository):
+        # Record Analyzer gets records from repository
         self.record_repo = record_repo
 
     def calculate_streak(self, habit):
+        """Calculates the current streak for a given habit."""
 
+        # Get all records for habit sorted according to their due date
         ref_records = self.record_repo.find_by_habit_id(habit.habit_id)
-
         ref_records_sorted = sorted(ref_records, key=lambda x: string_to_dt(x['due_date']), reverse=True)
 
+        # Count the number of completed records that are not overdue/skipped
+        # Stop counting when a skipped/overdue record is encountered
         streak = 0
         for record in ref_records_sorted:
             if record["completion_status"] == CompletionStatus.COMPLETED.value and record["was_overdue"] == 0:
@@ -49,11 +53,15 @@ class RecordAnalyzer:
         return streak
 
     def calculate_longest_streak(self, habit):
+        """Calculates the longest streak for a given habit."""
 
+        # Get all records for habit sorted according to their due date
         ref_records = self.record_repo.find_by_habit_id(habit.habit_id)
-
         ref_records_sorted = sorted(ref_records, key=lambda x: string_to_dt(x['due_date']), reverse=True)
 
+        # Count the number of completed records that are not overdue/skipped
+        # Restart streak count when a skipped/overdue record is encountered
+        # Append streak before restarting to list
         streak_list = []
         streak = 0
         for record in ref_records_sorted:
@@ -62,14 +70,18 @@ class RecordAnalyzer:
             else:
                 streak_list.append(streak)
                 streak = 0
-        streak_list.append(streak) # Possible that one streak is appended double, but this does not affect the correctness of the outcome
+        streak_list.append(streak)
+        # Possible that one streak is appended double, but this does not affect the correctness of the outcome
 
         return max(streak_list)
 
     def calculate_completion_rate(self, habit):
+        """Calculates the completion rate for a given habit."""
 
+        # Get all records for a given habit
         ref_records = self.record_repo.find_by_habit_id(habit.habit_id)
 
+        # Calculate ratio of completed records to total records
         total_records = len(ref_records)
         completed_records = sum(1 for record in ref_records if record['completion_status'] == 'Completed')
 
@@ -78,17 +90,24 @@ class RecordAnalyzer:
         return completed_records / total_records
 
     def calculate_finished_ontime_rate(self, habit):
+        """Calculates the on-time completion rate for a given habit."""
 
+        # Get all records for a given habit
         ref_records = self.record_repo.find_by_habit_id(habit.habit_id)
-        total_records = len(ref_records)
 
+        # Calculate ratio of completed records that were not overdue to total records
+        total_records = len(ref_records)
         ontime_records = sum(1 for record in ref_records if record['was_overdue'] == 0 and record['completion_status'] == 'Completed')
 
         if total_records == 0:
             return 0
         return ontime_records / total_records
 
+#------------NOT IMPLEMENTED YET------------
+
     def calculate_most_consistent_habit(self):
+        """Calculates the habit with the highest streak.
+        NOT IMPLEMENTED YET"""
         pass
 
         # Get all unique IDs in records list
@@ -100,4 +119,6 @@ class RecordAnalyzer:
         # return habit and longest streak for habit
 
     def habit_with_highest_completion_rate(self):
+        """Calculates the habit with the highest completion rate.
+        NOT IMPLEMENTED YET"""
         pass
